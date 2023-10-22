2d769747940bea3a4c20e01bdc88b27c17ce1497
Andres Martinez <andresmartinezzubiria@gmail.com>
2023-10-22 16:36:25 -0400

    Completed change preferred neighbors log function

--- peerProcess.java
+++ peerProcess.java
@@ -21,7 +21,8 @@ public class peerProcess {
         this.peerId = peerId;
     }
 
-    public static void logPeerConnection(int ID1, int ID2) {
+    // Log Method for TCP Connection
+    public static void logTCPConnection(int ID1, int ID2) {
 
         // Get the names of the log files
         String logFileName1 = "log_peer_" + ID1 + ".log";
@@ -39,7 +40,34 @@ public class peerProcess {
         logToLogFile(logFileName1, logMessage1);
         logToLogFile(logFileName2, logMessage2);
     }
+    
+    // Log Method for Change of Preferred Neighbors
+    public static void logPreferredNeighborsChange(int peerID, List<Integer> preferredNeighbors) {
+        // Get name of Log File
+        String logFileName = "log_peer_" + peerID + ".log";
 
+        // Get the current time in the desired format
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String currentTime = sdf.format(new Date());
+
+        // Build the log message with the list of preferred neighbors
+        StringBuilder preferredNeighborList = new StringBuilder();
+        for (int neighborID : preferredNeighbors) {
+            preferredNeighborList.append(neighborID).append(",");
+        }
+        // Remove the trailing comma
+        if (preferredNeighbors.size() > 0) {
+            preferredNeighborList.setLength(preferredNeighborList.length() - 1);
+        }
+
+        String logMessage = currentTime + ": Peer " + peerID + " has the preferred neighbors " + preferredNeighborList + ".";
+
+        // Log the message to the log file
+        logToLogFile(logFileName, logMessage);
+    }
+
+
+    // Method to write to the log files
     public static void logToLogFile(String fileName, String message) {
         // Specify the relative path for the log directory as an empty string since 
         // log files are in the same directory
@@ -167,6 +195,7 @@ public class peerProcess {
     }
 
     public static void main(String[] args) {
+         
         if (args.length != 1) {
             System.out.println("Usage: java peerProcess <peerID>");
             return;
@@ -183,6 +212,12 @@ public class peerProcess {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        /* HOW TO CALL logPreferredNeighborsChange EXAMPLE
+        int peerID = 1001;
+        List<Integer> preferredNeighbors = List.of(1002, 1003, 1004);
+        logPreferredNeighborsChange(peerID, preferredNeighbors);
+        */
     }
 }
 
